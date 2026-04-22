2ed4be88ed84b9349caf302794c38946f29e0d23
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-04-21 20:31:12 -0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>PixelProof</h1>
+      <button onClick={() => alert("App is working")}>
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
