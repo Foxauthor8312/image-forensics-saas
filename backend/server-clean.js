c5ecd3afce7ed27ea2a2296638324d22d0953a0e
Foxauthor8312 <lvrcav8tr@gmail.com>
2026-05-04 00:50:22 -0700

    Update server-clean.js

--- backend/server-clean.js
+++ backend/server-clean.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 10000;
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
 app.listen(PORT, () => {
-  console.log('Running on prt '+ PORT);
+  console.log('Running on port ' + PORT);
 });
